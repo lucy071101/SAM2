b98b510624b297ce0c7cf2bf5b62940ebd858a78
Lucy Oh <145899335+lucy071101@users.noreply.github.com>
2025-07-04 15:25:44 +0900

    Update comment build_sam2.py

--- build_sam2.py
+++ build_sam2.py
@@ -9,6 +9,8 @@ import logging
 import os
 
 import torch
+
+# hydra, omgaconf : 설정 파일을 처리하고 객체를 동적으로 생성하는 데 사용
 from hydra import compose
 from hydra.utils import instantiate
 from omegaconf import OmegaConf
@@ -16,8 +18,11 @@ from omegaconf import OmegaConf
 import sam2
 
 # Check if the user is running Python from the parent directory of the sam2 repo
+# 유저가 sam2 레포지토리에 있는 부모 디렉토리에서 실행하는지 확인
 # (i.e. the directory where this repo is cloned into) -- this is not supported since
 # it could shadow the sam2 package and cause issues.
+# sam2 패키지를 다른 이름이 덮어써서 불러오지 못해 이슈가 생길 수 있음
+# 패키지 이름과 디렉토리 이름이 충돌할 수 있음
 if os.path.isdir(os.path.join(sam2.__path__[0], "sam2")):
     # If the user has "sam2/sam2" in their path, they are likey importing the repo itself
     # as "sam2" rather than importing the "sam2" python package (i.e. "sam2/sam2" directory).
@@ -33,6 +38,8 @@ if os.path.isdir(os.path.join(sam2.__path__[0], "sam2")):
     )
 
 
+# HuggingFace 모델 ID와 파일 매핑
+# 다양한 모델 크기
 HF_MODEL_ID_TO_FILENAMES = {
     "facebook/sam2-hiera-tiny": (
         "configs/sam2/sam2_hiera_t.yaml",
@@ -70,14 +77,15 @@ HF_MODEL_ID_TO_FILENAMES = {
 
 
 def build_sam2(
-    config_file,
-    ckpt_path=None,
-    device="cuda",
-    mode="eval",
-    hydra_overrides_extra=[],
+    config_file, # YAML 설정 파일 경로
+    ckpt_path=None, # 모델 가중치 파일 경로
+    device="cuda", # 혹은 cpu로도 설정 가능
+    mode="eval", # 추론용이면 eval, 학습이면 train
+    hydra_overrides_extra=[], 설정을 일부 오버라이드 할 때 사용
     apply_postprocessing=True,
     **kwargs,
 ):
+    # dynamic_multimask 설정 추가 가능 : 하나의 이미지에 대해 여러 개의 마스크를 예측하는 기능
 
     if apply_postprocessing:
         hydra_overrides_extra = hydra_overrides_extra.copy()
@@ -89,15 +97,15 @@ def build_sam2(
         ]
     # Read config and init model
     cfg = compose(config_name=config_file, overrides=hydra_overrides_extra)
-    OmegaConf.resolve(cfg)
-    model = instantiate(cfg.model, _recursive_=True)
-    _load_checkpoint(model, ckpt_path)
-    model = model.to(device)
+    OmegaConf.resolve(cfg) # 설정 내부의 복잡한 값을 해석해주는 단계 (실제 값으로 바뀜)
+    model = instantiate(cfg.model, _recursive_=True) # 설정에서  _target_ 키가 명시된 클래스를 자동으로 인스턴스화 해줌
+    _load_checkpoint(model, ckpt_path) # 가중치 불러오기
+    model = model.to(device) # 모델을 gpu 혹은 cpu로 옮김
     if mode == "eval":
         model.eval()
     return model
 
-
+# 비디오용 SAM2 예측기
 def build_sam2_video_predictor(
     config_file,
     ckpt_path=None,
@@ -141,7 +149,7 @@ def build_sam2_video_predictor(
         model.eval()
     return model
 
-
+# HuggingFace에서 모델의 .yaml과 .pt 파일 자동 다운로드
 def _hf_download(model_id):
     from huggingface_hub import hf_hub_download
 
@@ -149,7 +157,7 @@ def _hf_download(model_id):
     ckpt_path = hf_hub_download(repo_id=model_id, filename=checkpoint_name)
     return config_name, ckpt_path
 
-
+# _hf_download로 불러온 모델과 설정 파일로 모델 초기화
 def build_sam2_hf(model_id, **kwargs):
     config_name, ckpt_path = _hf_download(model_id)
     return build_sam2(config_file=config_name, ckpt_path=ckpt_path, **kwargs)
@@ -161,11 +169,11 @@ def build_sam2_video_predictor_hf(model_id, **kwargs):
         config_file=config_name, ckpt_path=ckpt_path, **kwargs
     )
 
-
+# checkpoint 파일 로드
 def _load_checkpoint(model, ckpt_path):
     if ckpt_path is not None:
         sd = torch.load(ckpt_path, map_location="cpu", weights_only=True)["model"]
-        missing_keys, unexpected_keys = model.load_state_dict(sd)
+        missing_keys, unexpected_keys = model.load_state_dict(sd) # 가중치 불러오기
         if missing_keys:
             logging.error(missing_keys)
             raise RuntimeError()
